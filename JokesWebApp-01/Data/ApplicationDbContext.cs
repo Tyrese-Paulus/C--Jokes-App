6d9173120810d1a2de985299cffd9106a8f21408
tyrese-paulus <tpwebdevelopment11@gmail.com>
2021-08-04 21:30:46 +0200

    final-build

---
+++ JokesWebApp-01/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JokesWebApp_01.Models;
+
+namespace JokesWebApp_01.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebApp_01.Models.Joke> Joke { get; set; }
+    }
+}
